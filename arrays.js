f2805efdace59c7d07e74d71d6e2806a4958a675
Prateek Dwivedi <dprateek996@gmail.com>
2026-01-10 00:08:02 +0530

    Add arrays.js

---
+++ arrays.js
@@ -0,0 +1,43 @@
+const arr = [1, 2, 3, 4, 5];
+const newarr = [];
+/*
+for (let i =1; i<arr.length;i++){
+newarr.push([i] * 2);
+}   
+console.log(newarr);
+*/
+
+
+
+// const ans = arr.map(function (i){
+//     return i * 2;
+
+// });
+// console.log(ans); 
+
+// const ans2 = arr.map(function(i){
+//  return  arr[i] = i+1;
+// })
+// console.log(ans2);  
+
+// function myMap(arr, transformFn) {
+//   return arr.map(function (value, index, array) {
+//     return transformFn(value, index, array);
+//   });
+// }
+// const ans = myMap(arr, function (x) {
+//   return x + 1;
+// });
+          
+// console.log(ans);           (complete . map)
+
+// function myMapp (arrr, transformFnn ){
+//     return arr.map(function(value,index, array){
+//         return transformFnn(value, index, array);
+//     });
+// }
+// const anss  = myMapp (arr, function (y){
+//     return y +1;
+// });                            
+
+
