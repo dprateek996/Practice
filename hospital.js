4b1870d4623cf6973f039454a7251e00335d1cf2
Prateek Dwivedi <dprateek996@gmail.com>
2026-01-11 13:10:40 +0530

    route practice

---
+++ hospital.js
@@ -0,0 +1,81 @@
+const express = require ('express');
+const app = express();
+
+
+var users = [{
+    name: "John",
+     kidneys:[{
+        healthy:false
+     },{
+        healthy: true
+     }]
+}];
+
+app.use(express.json());
+
+app.get('/', function(req, res ){
+    const johnkidneys = users[0].kidneys;
+    const numberofKidneys = johnkidneys.length;
+    let numberofHealthyKidneys = 0;
+       for(let i = 0; i<johnkidneys.length; i++){
+        if (johnkidneys[i].healthy){
+            numberofHealthyKidneys = numberofHealthyKidneys + 1;
+        }
+       }
+        
+       const numberofUnhealthyKidneys = numberofKidneys - numberofHealthyKidneys;
+       res.json({
+        johnkidneys,
+        numberofHealthyKidneys,
+        numberofUnhealthyKidneys
+       })
+})
+
+app.post('/', function (req, res){
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done"
+    })
+})
+
+app.put("/", function (req, res){
+      for(let i = 0; i< users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy = true;
+      }
+      res.json({});
+})
+app.delete('/', function(req, res){
+
+if (isThereAtleastOneUnhealthyKidney()){const newKidneys = [];
+    for(let i =0; i<users[0].kidneys.length; i++){
+        if(users[0].kidneys[i].healthy){
+            newKidneys.push({
+                healthy: true
+            })
+        }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({msg: "done"})}
+    else{
+    res.status(411).json({
+        msg: "You have no bad kidneys"
+    }); 
+    }
+    
+    
+})
+
+ function isThereAtleastOneUnhealthyKidney (){
+    let atleastOneUnhealthyKidney = false;
+    for (let i =0; i< users[0].kidneys.length; i++){
+        if(!users[0].kidneys[i].healthy){
+            isThereAtleastOneHealthyKidney = true;
+        }
+    }
+    return atleastOneUnhealthyKidney
+ } ;
+
+app.listen(3000);
